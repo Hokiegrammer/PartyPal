cd82014b74f239e3aefdd4d4563f5d48090a81c9
kbuch5 <kbuch5@vt.edu>
2015-11-23 18:43:47 -0500

    Created a Home Screen and made the Invite button lead to the Invitation View.

---
+++ app/src/main/java/com/cs4634/group5/partypal/Home_Screen.java
@@ -0,0 +1,64 @@
+package com.cs4634.group5.partypal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Home_Screen extends AppCompatActivity
+{
+    Button inviteBtn;
+    Button findSuppliesBtn;
+    Button shoppingListBtn;
+    Button venueBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_home__screen);
+
+        inviteBtn = (Button)findViewById(R.id.inviteBtn);
+        findSuppliesBtn = (Button)findViewById(R.id.findSuppliesBtn);
+        shoppingListBtn = (Button)findViewById(R.id.shoppingListBtn);
+        venueBtn = (Button)findViewById(R.id.venueBtn);
+
+        inviteBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(v.getContext(), InvitationView.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        findSuppliesBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Find supplies activity.
+            }
+        });
+
+        shoppingListBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Shopping list activity.
+            }
+        });
+
+        venueBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Venue activity.
+            }
+        });
+    }
+}
